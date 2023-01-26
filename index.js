cefc283739aa0e65a3f6f9cf506eaddd68355d3d
Josephine Opar <josephineopar@gmail.com>
2023-01-26 17:07:55 +0200

    commit tree 0061b2c5c538a60c6ab179e6ef3b88fa32a484b1

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, driversName) {
+    return drivers.filter(
+        (member) => member.toLowerCase() === driversName.toLowerCase()
+    )
+}
+function fuzzyMatch(drivers, driversName) {
+    let partialMatch = drivers.filter(
+        (member) => member.substr(0, 2) === driversName
+    );
+    return partialMatch
+}
+
+const filtered = drivers.filter(function (value) {
+    return value === drivers
+});
+console.log(filtered)
+
+const driversTwo = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh'
+    },
+    {
+        name: 'Sammy',
+        hometown: 'New York'
+    },
+    {
+        name: 'Sally',
+        hometown: 'Cleveland'
+    },
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles'
+    },
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    }
+];
+function matchName(driversTwo, driversName) {
+    return driversTwo.filter((member) => member.name === driversName)
+}
+matchName(driversTwo)
